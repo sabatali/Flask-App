79d8ef8a83b011f02e92118180fb6502ecb94556
Sabat Ali <126473010+sabatali@users.noreply.github.com>
2025-02-07 01:04:45 +0500

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -91,7 +91,7 @@ def callback():
 @api.route("/refresh")
 def refresh():
     try:
-        refresh_token = "oauth2v2_ddc234203129130ec24b021613cfaeca"
+        refresh_token = "oauth2v2_4571f8f53eb6ffffff0da136b36c0e36"
         if not refresh_token:
             logger.error("No refresh token available")
             return jsonify({"status": "error", "message": "No refresh token available"}), 400
